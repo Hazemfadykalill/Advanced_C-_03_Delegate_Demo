7b16b0962cf784177976a94bfc8b2ac0c4e3ff57
Hazem Fady <hazemfady123@gmail.com>
2024-08-04 21:56:43 +0300

    Add project files.

---
+++ Advanced_C#_03_Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using static Advanced_C__03_Delegate.StringsFunctions;
+
+namespace Advanced_C__03_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Delegate [Example 01]
+            //GetCountFuncUpper x = StringsFunctions.GetCountOfUpperCharacter;//syntax Sugar
+            ////GetCountFuncUpper x =new GetCountFuncUpper(StringsFunctions.GetCountOfUpperCharacter) ;
+
+            ////x ==> Become Function Same Function StringsFunctions.GetCountOfUpperCharacter();
+            //// X ==> Become Refer To Another Function Same Signature this Delegate
+            //int Count = x("Hello World");// or int Count = x.Invoke("Hello World");
+            //Console.WriteLine(Count);
+
+
+            ////To X refer to another function
+            //x += StringsFunctions.GetCountOfLowerCharacter;
+            //int Count02 = x("Hello World");
+            //Console.WriteLine(Count02); 
+            #endregion
+        }
+    }
+}
---
+++ Advanced_C#_03_Delegate/StringsFunctions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_C__03_Delegate
+{
+    public class StringsFunctions
+    {
+        public delegate int GetCountFuncUpper(string word);
+        public static int GetCountOfUpperCharacter(string Word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(Word))
+            {
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    if (char.IsUpper(Word[i]))
+                    {
+                        count++;
+                    }
+
+                }
+                return count;
+
+           }
+            return count;
+        }
+
+        public static int GetCountOfLowerCharacter(string Word)
+        {
+            int count = 0;
+            if (!string.IsNullOrEmpty(Word))
+            {
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    if (char.IsLower(Word[i]))
+                    {
+                        count++;
+                    }
+
+                }
+                return count;
+
+            }
+            return count;
+        }
+    }
+}
